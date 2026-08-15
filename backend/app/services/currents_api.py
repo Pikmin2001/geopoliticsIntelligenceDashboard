b864434b0421d226d6657af6929b7bc19d8bb008
Pikmin2001 <joshua.wallace311@gmail.com>
2026-08-15 00:24:42 -0400

    Fix news

--- backend/app/services/currents_api.py
+++ backend/app/services/currents_api.py
@@ -2,25 +2,25 @@ import requests
 
 from app.config import settings
 
-
 def get_news():
     params = {
         "language": "en",
         "page_size": 10,
-        "keywords": "Ukraine"
+        "keywords": "Ukraine",
     }
-
-    print("NEWS PARAMS:", params)
 
     response = requests.get(
         settings.base_url_news,
         params=params,
-        headers={"Authorization": settings.api_key_news},
-        timeout=10
+        headers={
+            "Authorization": settings.api_key_news
+        },
+        timeout=10,
     )
 
-    print("NEWS URL:", response.url)
-    print("NEWS RESPONSE:", response.json())
+    print("URL:", response.url)
+    print("STATUS:", response.status_code)
+    print("RESPONSE:", response.text)
 
     response.raise_for_status()
 
