6ceb0d4077fbffffed4eae2543fb6fc442b8de05
Pikmin2001 <joshua.wallace311@gmail.com>
2026-08-14 23:02:20 -0400

    Update news query

--- backend/app/services/currents_api.py
+++ backend/app/services/currents_api.py
@@ -5,7 +5,7 @@ from app.config import settings
 
 def get_news():
     response = requests.get(
-        f"{settings.base_url_news}", params={"language": "en", "page_size": 4, "keywords": "Iran"}, 
+        f"{settings.base_url_news}", params={"language": "en", "page_size": 4, "keywords": "Trump"}, 
         headers={"Authorization": settings.api_key_news}, timeout=10
         )
     
